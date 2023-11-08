e315510cdd11a9c1911097c18c05c7e700ef8dd2
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2023-11-08 10:14:02 +0700

    testing with scalabilityMode S3T3

--- public/javascript/room/config/mediasoup/index.js
+++ public/javascript/room/config/mediasoup/index.js
@@ -1,19 +1,19 @@
 let params = {
 	encodings: [
 		{
-			rid: 'r0',
 			maxBitrate: 300000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 4
 		},
 		{
-			rid: 'r1',
 			maxBitrate: 500000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 2
 		},
 		{
-			rid: 'r2',
 			maxBitrate: 700000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 1
 		},
 	],
 	codecOptions: {
--- public/javascript/room/roombundle.js
+++ public/javascript/room/roombundle.js
@@ -21841,19 +21841,19 @@ module.exports = function (session, opts) {
 let params = {
 	encodings: [
 		{
-			rid: 'r0',
 			maxBitrate: 300000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 4
 		},
 		{
-			rid: 'r1',
 			maxBitrate: 500000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 2
 		},
 		{
-			rid: 'r2',
 			maxBitrate: 700000,
-			scalabilityMode: "S1T3",
+			scalabilityMode: "S3T3",
+			scaleResolutionDownBy: 1
 		},
 	],
 	codecOptions: {
