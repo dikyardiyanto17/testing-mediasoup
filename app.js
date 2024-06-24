adde0131b1d5cf3f16627c7fe8bbbe2c0dd774ca
dikyardiyanto17 <dikyardiyanto17@gmail.com>
2024-06-24 13:53:59 +0700

    adding setIncoming and setOutcoming bit rate

--- app.js
+++ app.js
@@ -143,8 +143,8 @@ io.on("connection", async (socket) => {
 		try {
 			let router = serverParameter.allRooms[roomName].router
 			const transport = await createWebRtcTransport({ router, serverParameter })
-			// transport.setMaxIncomingBitrate(1500000)
-			// transport.setMaxOutgoingBitrate(1500000)
+			transport.setMaxIncomingBitrate(1500000)
+			transport.setMaxOutgoingBitrate(1500000)
 			let username
 			const editParticipants = serverParameter.allRooms[roomName].participants.map((data) => {
 				if (data.socketId == socket.id) {
